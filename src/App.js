72499bfee0a65446aeb6e9e8ae5034e56f359cd9
HeinerAlejandro <heiner.enis@gmail.com>
2021-10-11 22:08:22 -0400

    Added Subcomponents from TaskOrganized to App

--- src/App.js
+++ src/App.js
@@ -4,11 +4,16 @@ import logo from './logo.svg';
 import './App.css';
 
 import Header from "./components/Header";
+import Calendar from "./components/TaskOrganized/Calendar"
+import BoxInput from "./components/TaskOrganized/BoxInput";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Calendar />
+      <BoxInput />
+
     </div>
   );
 }
