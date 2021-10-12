00391cd8d51fe5dd1945f0e5dee2e4a18b619885
HeinerAlejandro <heiner.enis@gmail.com>
2021-10-11 20:01:13 -0400

    Template TaskOrganized Component.
    - Added Template BoxInput Component.
    - Added Template Calendar Component.

---
+++ src/components/TaskOrganized/BoxInput/index.js
@@ -0,0 +1,11 @@
+import {Form} from "react-bootstrap";
+
+const BoxInput = () => {
+    return (
+        <Form>
+            <caption class = "d-block text-center">Este es el box de personalizacion de agregado de tarea</caption>
+        </Form>
+    )
+}
+
+export default BoxInput
---
+++ src/components/TaskOrganized/Calendar/index.js
@@ -0,0 +1,9 @@
+const Calendar = () => {
+    return (
+        <p>
+            Este es el calendario
+        </p>
+    )
+}
+
+export default Calendar
